2804820b0de553c0fa400d0abed2433627cbe41e
114096-GALLO-JUAN-IGNACIO <114096@tecnicatura.frc.utn.edu.ar>
2025-01-14 02:47:59 -0300

    - Added ng material and used it to set up some inputs and icons

--- src/app/components/register-user/register-user.component.ts
+++ src/app/components/register-user/register-user.component.ts
@@ -1,10 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon'
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+// import {MatIconModule} from '@angular/material/icon';
+// import {MatInputModule} from '@angular/material/input';
+
 
 @Component({
   selector: 'tc-register-user',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, MatInputModule, MatIconModule, 
+    MatButtonModule, MatFormFieldModule
+  ],
   templateUrl: './register-user.component.html',
   styleUrl: './register-user.component.css'
 })
@@ -13,14 +22,26 @@ export class RegisterUserComponent implements OnInit{
   constructor(private formBuilder: FormBuilder) {}
   
   registerForm!: FormGroup;
+  hidePasswordSignal = signal(false);
+  hideConfirmationPasswordSignal = signal(false);
   
   ngOnInit(): void {
     this.registerForm = this.formBuilder.group({
       username: [],
       email: [],
       password: [],
-      passwordConfirmation: []
+      confirmationPassword: []
     });
+  }
+  
+  hidePassword(event: MouseEvent) {
+    this.hidePasswordSignal.set(!this.hidePasswordSignal());
+    event.stopPropagation();
+  }
+  
+  hideConfirmationPassword(event: MouseEvent) {
+    this.hideConfirmationPasswordSignal.set(!this.hideConfirmationPasswordSignal());
+    event.stopPropagation();
   }
 
   submit(): void {
