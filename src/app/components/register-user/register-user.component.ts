a8b0b08b98466f6c73ecc0309735ed92f1f2e9b5
114096-GALLO-JUAN-IGNACIO <114096@tecnicatura.frc.utn.edu.ar>
2025-01-14 16:59:35 -0300

    - Made form responsive on smaller laptop screens

--- src/app/components/register-user/register-user.component.ts
+++ src/app/components/register-user/register-user.component.ts
@@ -2,24 +2,25 @@ import { Component, OnInit, signal } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon'
-import {MatButtonModule} from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
-// import {MatIconModule} from '@angular/material/icon';
-// import {MatInputModule} from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'tc-register-user',
   standalone: true,
-  imports: [ReactiveFormsModule, MatInputModule, MatIconModule, 
-    MatButtonModule, MatFormFieldModule
-  ],
+  imports: [ReactiveFormsModule, MatInputModule, MatIconModule, MatButtonModule, MatFormFieldModule, CommonModule],
   templateUrl: './register-user.component.html',
   styleUrl: './register-user.component.css'
 })
 export class RegisterUserComponent implements OnInit{
 
-  constructor(private formBuilder: FormBuilder) {}
+  isLargeScreen: boolean = false;
+
+  constructor(private formBuilder: FormBuilder, 
+    private breakpointObserver: BreakpointObserver) {}
   
   registerForm!: FormGroup;
   hidePasswordSignal = signal(false);
@@ -32,6 +33,26 @@ export class RegisterUserComponent implements OnInit{
       password: [],
       confirmationPassword: []
     });
+
+    this.breakpointObserver.observe([Breakpoints.XLarge, Breakpoints.Large, Breakpoints.Small, Breakpoints.Handset]).subscribe(result => {
+      if(result.breakpoints[Breakpoints.XLarge]) {
+        // X-LARGE
+        this.isLargeScreen = false;
+        console.log("X-LARGE");
+      } else if (result.breakpoints[Breakpoints.Large]) {
+        // LARGE
+        this.isLargeScreen = true;
+        console.log("LARGE");
+      } else if (result.breakpoints[Breakpoints.Small]) {
+        // SMALL
+        this.isLargeScreen = false;
+        console.log("SMALL");
+      } else if (result.breakpoints[Breakpoints.Handset]) {
+        // HANDSET
+        this.isLargeScreen = false;
+        console.log("HANDSET");
+      }
+    })
   }
   
   hidePassword(event: MouseEvent) {
