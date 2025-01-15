10b47d107075416c9513a56f52330a70dc1b9ead
114096-GALLO-JUAN-IGNACIO <114096@tecnicatura.frc.utn.edu.ar>
2025-01-15 01:44:37 -0300

    - Added temporal lazy loads (may change later)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { RegisterUserComponent } from './components/register-user/register-user.component';
+import { LoginUserComponent } from './components/login-user/login-user.component';
 
 export const routes: Routes = [
   {
     path: '', redirectTo: 'register', pathMatch: "full"
-  },
-  {
-    path: 'register', component: RegisterUserComponent
+  }, {
+    path: 'register', loadComponent: () => import('./components/register-user/register-user.component').then(c => c.RegisterUserComponent)
+  }, {
+    path: 'login', loadComponent: () => import('./components/login-user/login-user.component').then(c => c.LoginUserComponent)
   }
 ];
