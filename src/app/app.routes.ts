c2a5dfb7c8be304965e9987f5915542c53d48ebd
114096-GALLO-JUAN-IGNACIO <114096@tecnicatura.frc.utn.edu.ar>
2025-01-15 01:37:38 -0300

    - Added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { RegisterUserComponent } from './components/register-user/register-user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', redirectTo: 'register', pathMatch: "full"
+  },
+  {
+    path: 'register', component: RegisterUserComponent
+  }
+];
