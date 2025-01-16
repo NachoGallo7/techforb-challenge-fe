77b1e788e3c3544eec4588b5170e94bd86bdc83f
114096-GALLO-JUAN-IGNACIO <114096@tecnicatura.frc.utn.edu.ar>
2025-01-16 18:27:03 -0300

    - Sidebar progress

--- src/app/components/plants-dashboard/plants-dashboard.component.ts
+++ src/app/components/plants-dashboard/plants-dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PlantsDashboardSidebarComponent } from '../plants-dashboard-sidebar/plants-dashboard-sidebar.component';
 
 @Component({
   selector: 'tc-plants-dashboard',
   standalone: true,
-  imports: [],
+  imports: [PlantsDashboardSidebarComponent],
   templateUrl: './plants-dashboard.component.html',
   styleUrl: './plants-dashboard.component.css'
 })
