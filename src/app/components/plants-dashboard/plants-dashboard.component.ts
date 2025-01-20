78f06befaa93a7602643b1c1efc9c91097d0b7d1
114096-GALLO-JUAN-IGNACIO <114096@tecnicatura.frc.utn.edu.ar>
2025-01-20 09:28:26 -0300

    - Made dashboard responsive

--- src/app/components/plants-dashboard/plants-dashboard.component.ts
+++ src/app/components/plants-dashboard/plants-dashboard.component.ts
@@ -8,11 +8,13 @@ import { EditPlantModalComponent } from "../edit-plant-modal/edit-plant-modal.co
 import { PlantService } from '../../services/plant.service';
 import { PlantDTO } from '../../models/plants';
 import { BehaviorSubject } from 'rxjs';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'tc-plants-dashboard',
   standalone: true,
-  imports: [GlobalKpiIndicatorComponent, DashboardPlantsTableComponent, DashboardPerPlantDetailComponent, AddPlantModalComponent, DashboardPerPlantDetailComponent],
+  imports: [GlobalKpiIndicatorComponent, DashboardPlantsTableComponent, DashboardPerPlantDetailComponent, AddPlantModalComponent, DashboardPerPlantDetailComponent, CommonModule],
   templateUrl: './plants-dashboard.component.html',
   styleUrl: './plants-dashboard.component.css'
 })
@@ -27,7 +29,19 @@ export class PlantsDashboardComponent implements OnInit{
   globalAlerts: WritableSignal<number|undefined> = signal(undefined);
   globalDisabledSensors: WritableSignal<number|undefined> = signal(undefined);
 
-  constructor(private plantService: PlantService){}
+  breakpointLevel = signal(1);
+  breakpointLevelMap: {[breakpointName: string]: number} = {
+    [Breakpoints.XLarge]:   5,
+    [Breakpoints.Large]:    4,
+    [Breakpoints.Medium]:   3,
+    [Breakpoints.Small]:    2,
+    [Breakpoints.Handset]:  1
+  }
+  
+
+  constructor(private plantService: PlantService,
+    private breakpointObserver: BreakpointObserver
+  ){}
 
   ngOnInit(): void {
     this.plantService.plants$.subscribe({
@@ -37,6 +51,30 @@ export class PlantsDashboardComponent implements OnInit{
       }
     });
     this.plantService.fetchAll();
+
+    this.breakpointObserver.observe([Breakpoints.XLarge, Breakpoints.Large, Breakpoints.Medium, Breakpoints.Small, Breakpoints.Handset]).subscribe(result => {
+      if(result.breakpoints[Breakpoints.XLarge]) {
+        // X-LARGE
+        this.breakpointLevel.set(this.breakpointLevelMap[Breakpoints.XLarge]);
+        console.log("X-LARGE");
+      } else if (result.breakpoints[Breakpoints.Large]) {
+        // LARGE
+        this.breakpointLevel.set(this.breakpointLevelMap[Breakpoints.Large]);
+        console.log("LARGE");
+      } else if (result.breakpoints[Breakpoints.Medium]) {
+        // MEDIUM
+        this.breakpointLevel.set(this.breakpointLevelMap[Breakpoints.Medium]);
+        console.log("MEDIUM");
+      } else if (result.breakpoints[Breakpoints.Small]) {
+        // SMALL
+        this.breakpointLevel.set(this.breakpointLevelMap[Breakpoints.Small]);
+        console.log("SMALL");
+      } else if (result.breakpoints[Breakpoints.Handset]) {
+        // HANDSET
+        this.breakpointLevel.set(this.breakpointLevelMap[Breakpoints.Handset]);
+        console.log("HANDSET");
+      }
+    })
   }
 
   updateGlobalReadings(): void {
@@ -58,6 +96,11 @@ export class PlantsDashboardComponent implements OnInit{
 
   selectPlant(plant: PlantDTO): void {
     this.selectedPlant.next(plant);
+  }
+  
+
+  get Breakpoints() {
+    return Breakpoints;
   }
 
   get globalKpiStates(){
